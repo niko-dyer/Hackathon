4e497e616b242a7156b4613ab2adc1dff1ca01ea
Niko Dyer <nikodyer44@gmail.com>
2019-03-09 09:57:28 -0700

    built trips controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  get 'addresses/index'
-  get 'addresses/show'
-  get 'addresses/new'
-  get 'addresses/edit'
-  get 'locations/index'
-  get 'locations/show'
-  get 'locations/new'
-  get 'locations/edit'
-  get 'trips/index'
-  get 'trips/show'
-  get 'trips/new'
-  get 'trips/edit'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'trips#index'
+
+  resources :trips do
+    resources :locations
+  end
+
+  resources :locations do
+    resources :addresses
+  end
 end
