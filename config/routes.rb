1c5d3b9cadc4458347ef62d88943efe7f9a74d95
Niko Dyer <nikodyer44@gmail.com>
2019-03-09 09:46:17 -0700

    models and controllers built

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'addresses/index'
+  get 'addresses/show'
+  get 'addresses/new'
+  get 'addresses/edit'
+  get 'locations/index'
+  get 'locations/show'
+  get 'locations/new'
+  get 'locations/edit'
+  get 'trips/index'
+  get 'trips/show'
+  get 'trips/new'
+  get 'trips/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
