d0ca169031fdf8b752cc85950cc77680abd45fae
Joshua Larsen <larsen.bass@gmail.com>
2019-03-09 11:33:23 -0700

    change trip controller to use current_user

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,7 +2,7 @@ class TripsController < ApplicationController
   before_action :set_trip, only: [:show, :edit, :update, :destroy]
 
   def index
-    @trips = Trip.all
+    @trips = current_user.trips
   end
 
   def show
@@ -14,7 +14,7 @@ class TripsController < ApplicationController
   end
 
   def create
-    @trip = Trip.new(trip_params)
+    @trip = current_user.trips.new(trip_params)
 
     if @trip.save
       redirect_to trips_path
@@ -41,7 +41,7 @@ class TripsController < ApplicationController
 
   private
     def set_trip
-      @trip = Trip.find(params[:id])
+      @trip = current_user.trips.find(params[:id])
     end
 
     def trip_params
